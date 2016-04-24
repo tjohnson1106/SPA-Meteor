a9c2bdd5fe8e36fce80c23f85c1f8d7bf1072b56
Thomas Johnson <tjohnson1106@gmail.com>
2016-04-24 18:35:54 -0400

    displayed data with template helpers

---
+++ client/templates/home.js
@@ -0,0 +1,16 @@
+ Template.home.created = function() {
+ 	console.log('Created the home template');
+ };
+
+ Template.home.rendered = function() {
+ 	console.log('Rendered the home template');
+ };
+ Template.home.destroyed = function() {
+ 	console.log('Destroyed the home template');
+ };
+
+ Template.home.helpers({
+ 	exampleHelper: function() {
+ 		return 'This text came with a helper with some <strong>HTML</strong>.';                                            
+ 	}
+ });
